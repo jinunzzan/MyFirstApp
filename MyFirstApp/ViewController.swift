1b1e9099104a955aaddce2b0a0862e8b02460164
jinunzzan <lovebox6607@gmail.com>
2022-10-25 01:44:05 +0900

    addSubView 코드로 뷰 추가하기

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -11,7 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let view1 = UIView()
+        
+        view1.frame = CGRect(x: 100, y: 100, width: 200, height: 200)
+        view1.backgroundColor = UIColor(red: 1, green: 1, blue: 0, alpha: 1)
+        view.addSubview(view1)
+        
+        let rect1:CGRect = CGRect(x: 100, y: 200, width: 200, height: 50)
+        let label = UILabel(frame: rect1)
+        label.backgroundColor = UIColor.cyan
+        label.text = "복습하는 중"
+        label.textAlignment = .center
+        view.addSubview(label)
     }
 
 

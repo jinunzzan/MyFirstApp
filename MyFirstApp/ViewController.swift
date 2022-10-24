c953b9472f406adee95bb1336da9c6ba199b2d08
jinunzzan <lovebox6607@gmail.com>
2022-10-25 01:38:41 +0900

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Eunchan Kim on 2022/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
